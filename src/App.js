c3b0e4fd6216891822428487bf18b88cbadd7c9f
DylDarab <apicha.oap@gmail.com>
2022-04-04 22:09:59 +0700

    init react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import './style.css';
-import Navbar from './component/Navbar'; 
-import About from './component/About';       
+import './style.css'
+import Navbar from './component/Navbar'
+import About from './component/About'
+import Contactus from './component/Contactus'
+import Service from './component/Service'
+import Blognnew from './component/Blognnew'
+import
+  {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom"
 
-const App = () => {
+const App = () =>
+{
   return (
-    <div>
-      <Navbar />
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <Navbar/>
+            <About />
+          </div>} />
+        <Route path="/contactus" element={
+          <div>
+            <Navbar/>
+            <Contactus />
+          </div>} />
+        <Route path="/service" element={
+          <div>
+            <Navbar/>
+            <Service />
+          </div>} />
+        <Route path="/blognnew" element={
+          <div>
+            <Navbar/>
+            <Blognnew />
+          </div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,15 +1,23 @@
-import Logo from "../assets/images/logo.jpg";
-
+import Logo from "../assets/images/logo.jpg"
+//import reactrouterdom
+import {useNavigate} from "react-router-dom"
 const Navbar = () => {
+
+  const navigate = useNavigate()
+
+  const onChangePage = (page)=>{
+    navigate(page)
+  }
+
   return (
     <div>
       <div className="navbar">
         <img src={Logo} alt="logo" id="logo" />
         <ul className="menu-bar">
-          <li>ABOUT US</li>
-          <li>SERVICE</li>
-          <li>BLOG & NEWS</li>
-          <li>CONTACT US</li>
+          <li onClick={() => onChangePage('/')}>ABOUT US</li>
+          <li onClick={() => onChangePage('/service')}>SERVICE</li>
+          <li onClick={() => onChangePage('/blognnews')}>BLOG & NEWS</li>
+          <li onClick={() => onChangePage('/contactus')}>CONTACT US</li>
         </ul>
       </div>
     </div>
